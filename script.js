21c901d593377dd04c10d799e2c130068b28be74
NA Flipper <dioncleung@gmail.com>
2020-03-24 09:36:31 -0700

    embedded a twitter follow button

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ $(".img-thumbnail-DCL").on("mouseleave", function(){
     var thumbnailLabel = $(this).next()
     thumbnailLabel.css("display", "none")
 })
+
+// $(document).on("click", ".thumbnail-container-DCL", function(){
+//     var linkTarget = $(this).children().next();
+//     var linkDestination = linkTarget.attr("href")
+//     window.location.href = linkDestination
+// })
